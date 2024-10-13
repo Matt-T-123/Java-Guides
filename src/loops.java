58fc9832fcab0be8e14aa4564c4d5261d30538de
Matt.T <49764813+Matt-T-123@users.noreply.github.com>
2024-10-13 21:05:49 +0100

    Add files via upload

---
+++ src/loops.java
@@ -0,0 +1,19 @@
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		while(true) { // while(this condition is met) run everything inside || You can also use an iterator as a condition
+			//will be your code that will run as long as the condition above is met
+		}
+		 
+		
+		for (int i = 0; i < 5; i++) { //for (loop starter, condition needed to exit for loop, every time loop is ran run this code)
+			//Every time the condition above is satisfied, the code in here will run 
+		}
+		
+		for (String string : args) { //for (each item in a set of items)
+			//Every time there is an item in the set of items, the code in here will run
+		}
+	}
+}
---
+++ src/printing.java
@@ -0,0 +1,11 @@
+
+public class printing {
+
+	public static void main(String[] args) {
+		System.out.print("Print to current line");
+		System.out.println("Prints text then stops you writing to current line");
+		String line = "prints string";
+		System.out.printf("%s", line); //More info at https://docs.oracle.com/en/java/javase/20/docs/api/java.base/java/util/Formatter.html#conversions-heading
+	}
+
+}
